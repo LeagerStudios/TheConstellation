3aa7563f5100975593d41e9efbfc75542e3e3076
Haritz5837 <161754912+Haritz5837@users.noreply.github.com>
2026-09-18 18:52:58 +0200

    Minigame1 Part1

---
+++ Assets/Scripts/M1/PlayerM1.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerM1 : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float force;
+    public float maxSpeed;
+    public Vector2 startPos;
+    private bool keyPressed;
+    public KeyCode keyCode;
+    public float maxAngle;
+    public float SpeedOfMaxAngle;
+
+
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        keyPressed = false;
+        transform.position = startPos;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(keyCode))
+        {
+            keyPressed = true;
+        }
+        else
+        {
+            keyPressed = false;
+        }
+
+        if(rb.velocity.y<SpeedOfMaxAngle && rb.velocity.y > -SpeedOfMaxAngle)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * maxAngle / SpeedOfMaxAngle);
+        }
+        else
+        {
+            if(rb.velocity.y > SpeedOfMaxAngle)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, maxAngle);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, -maxAngle);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(keyPressed)
+        {
+            rb.AddForce(new Vector2(0, force));
+        }
+
+        if(rb.velocity.y > maxSpeed)
+        {
+            rb.velocity = new Vector2(0, maxSpeed);
+        }
+
+        if (rb.velocity.y < -maxSpeed)
+        {
+            rb.velocity = new Vector2(0, -maxSpeed);
+        }
+    }
+}
