c6ca9912101239631978d336f432ad698d4e6735
Haritz5837 <161754912+Haritz5837@users.noreply.github.com>
2026-09-18 19:12:44 +0200

    Minigame1 Part2

---
+++ Assets/Scripts/M1/Asteroid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        if(transform.position.x <= -9)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+    }
+}
---
+++ Assets/Scripts/M1/AsteroidGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    public GameObject AsteroidPrefab;
+    public float Xpos;
+    public float maxYpos, minYpos;
+    public float waitTime;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(GenerateAsteroid), 0f, waitTime);
+    }
+
+    void GenerateAsteroid()
+    {
+        float Ypos = Random.Range(minYpos, maxYpos);
+
+        Instantiate(AsteroidPrefab, new Vector2(Xpos, Ypos), Quaternion.identity);
+    }
+}
