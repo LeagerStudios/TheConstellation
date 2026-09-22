f29b974749d2736afcd96d3fa80fc63aa90ffff1
MrLokin <davidgl231226@gmail.com>
2026-09-22 18:59:37 +0200

    Wachito pau pau

---
+++ Assets/Scripts/Game/SettingsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsManager : MonoBehaviour
+{
+
+    //Components and stuff
+    AudioManager a_manager;
+
+    //Variables
+    float volume;
+
+    void ChangeVolume_BGM()
+    {
+
+
+
+    }
+
+}
